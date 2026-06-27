22113787bbc8eb86c4f2ac46b7c32d5bc5cecba1
valbac <eztore1412@gmail.com>
2026-06-27 14:55:29 +0200

    red

--- src/test/kotlin/ShoppingCartTest.kt
+++ src/test/kotlin/ShoppingCartTest.kt
@@ -7,9 +7,16 @@ private lateinit var shoppingCart: ShoppingCart
 
 class ShoppingCartTest {
     @Test
-    fun `test_empty_cart_has_zero_total`(){
+    fun test_empty_cart_has_zero_total(){
         shoppingCart = ShoppingCart()
 
         assertEquals(0, shoppingCart.getTotalItems().size)
+    }
+
+    @Test
+    fun test_add_item_increases_total(){
+        val firstItem = ShoppingCart().addItem()
+
+        assertEquals(1, shoppingCart.getTotalItems().size)
     }
 }
