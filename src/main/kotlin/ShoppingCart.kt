a20cdab31572bc6e936dc2b14b747732d21c1195
valbac <eztore1412@gmail.com>
2026-06-27 14:48:45 +0200

    (red war bereits im initial commit) green

--- src/main/kotlin/ShoppingCart.kt
+++ src/main/kotlin/ShoppingCart.kt
@@ -1,4 +1,10 @@
 package org.example
 
+import kotlin.collections.List
+
+data class Item(val name: String)
 class ShoppingCart {
+    private val items = mutableListOf<Item>()
+
+    fun getTotalItems(): List<Item> = items.toList()
 }
